9dff738c7b4c407aa87740008979756f771d72e9
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2025-07-12 23:24:01 +0300

    Set Request - Response models for accessing NutritionIx API

---
+++ NutritionProject/Domain/GetNutritionData/AltMeasure.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Domain.GetNutritionData
+{
+    public class AltMeasure
+    {
+        [JsonPropertyName("servingWeight")]
+        public double? ServingWeight { get; set; }
+
+        [JsonPropertyName("measure")]
+        public string Measure { get; set; }
+
+        [JsonPropertyName("seq")]
+        public int? Seq { get; set; }
+
+        [JsonPropertyName("qty")]
+        public int? Qty { get; set; }
+    }
+}
