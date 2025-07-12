032f451c9ee7f962406993d3f759dab946d5b7c9
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2025-07-13 00:22:17 +0300

    Create Request - Response DTOs

---
+++ NutritionProject/Application/NutritionService/NutritionService.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.NutritionService
+{
+    public class NutritionService
+    {
+        
+    }
+}
---
+++ NutritionProject/Domain/GetNutritionDataDTOs/FoodDto.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Domain.GetNutritionDataDTOs
+{
+    public class FoodDto
+    {
+        [JsonPropertyName("foodName")]
+        public string FoodName { get; set; } = string.Empty;
+
+        [JsonPropertyName("calories")]
+        public double? Calories { get; set; }
+
+        [JsonPropertyName("carbohydrates")]
+        public double? Carbohydrates { get; set; }
+
+        [JsonPropertyName("protein")]
+        public double? Protein { get; set; }
+
+        [JsonPropertyName("totalFat")]
+        public double? TotalFat { get; set; }
+
+        [JsonPropertyName("servingDescription")]
+        public string ServingDescription { get; set; } = string.Empty;
+
+        [JsonPropertyName("full_nutrients")]
+        public List<FullNutrientDto> FullNutrients { get; set; } = string.Empty;
+    }
+}
