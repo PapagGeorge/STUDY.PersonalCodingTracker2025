55bef8fced9792a5fdd9224129f580b0986b2646
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2025-08-07 00:26:40 +0300

    AuthService Initial Create

---
+++ AuthService/Domain/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Domain
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
