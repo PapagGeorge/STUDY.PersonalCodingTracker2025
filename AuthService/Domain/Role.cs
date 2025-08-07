5e885b23b1c6a8585ab6a01419afba11c7088332
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2025-08-07 04:25:16 +0300

    Implementation of IRoleRepository

--- AuthService/Domain/Role.cs
+++ AuthService/Domain/Role.cs
@@ -9,6 +9,8 @@ namespace Domain
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime CreatedAt { get; set; }
+        public bool IsRevoked { get; set; } = false;
+        public DateTime? RevokedAt { get; set; }
 
         //Navigation Properties
         public virtual ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
